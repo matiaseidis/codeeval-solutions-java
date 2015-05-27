ab09b871658f070f1d2d8d0a36e158cc9d991652
matiaseidis <matiaseidis@gmail.com>
2015-05-26 21:23:25 -0300

    ongoing

---
+++ src/test/java/LongestCommonSubsequenceTest.java
@@ -0,0 +1,15 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * Created by us on 25/05/15.
+ */
+public class LongestCommonSubsequenceTest {
+
+    @Test
+    public void test() throws IOException {
+        LongestCommonSubsequence.main(new String[]{"src/main/resources/longest-common-subsequence"});
+    }
+}
